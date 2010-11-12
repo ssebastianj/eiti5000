015ad914459324defea2a4ff9b8069d471221f12
Sebastian Seba <ssebastianj@gmail.com>
2010-11-12 11:58:48 -0300

    Actualizado __version__ de 1.2 a 1.3

--- src/marcador.py
+++ src/marcador.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Sebastián J. Seba"
-__version__ = "1.2"
+__version__ = "1.3"
 __license__ = """This program is free software: you can redistribute it and/or
                  modify it under the terms of the GNU General Public License
                  as published by the Free Software Foundation, either version
